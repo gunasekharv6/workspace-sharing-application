2140df0e482ccd880a3e18d30b5e5bd4bbcff4c9
kinjalthakkar1408 <91446577+kinjalthakkar1408@users.noreply.github.com>
2022-04-27 00:04:10 -0400

    adding location page

---
+++ frontend/src/pages/Locations/index.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import {getAllCombo} from "../../services/palnServices";
+import PublicComboPreview from "../../components/PublicComboPreview";
+import './layout.scss'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+    class Locations extends Component {
+    state={comboList:[]};
+
+    //Get all combos
+    initState=async ()=>{
+        const user=localStorage.getItem("user");
+        const {data}=await getAllCombo();
+        this.setState({comboList:data});
+    }
+
+    componentDidMount() {
+        this.initState();
+    }
+
+    render() {
+        const {comboList}=this.state;
+        return (
+            <div className="ebg">
+
+                <ul className="card-list">
+                    {
+                        comboList.map((combo)=>{
+                            return <PublicComboPreview key={combo._id} {...combo}/>;
+                        })
+                    }
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Locations;
